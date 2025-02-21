7e8198fff79991bb095706c263bf6668cbae8ddf
hermes <mercury.iwork@gmail.com>
2025-02-22 00:12:29 +0800

    feat(response): response structure

--- src/utils/k-means.js
+++ src/utils/k-means.js
@@ -1,15 +1,14 @@
 const { kmeans } = require('ml-kmeans');
 
-module.exports = (sentence, clusterCount, embeddings) => {
+module.exports = (sentences, clusterCount, embeddings) => {
     const clusters = kmeans(embeddings, clusterCount, {
         initialization: "random",
     }).clusters;
 
-    return clusters.reduce((previousValue, currentValue, currentIndex) => {
-        const group = previousValue[currentValue] ?? [];
-        group.push(sentence[currentIndex]);
-
-        previousValue[currentValue] = group;
-        return previousValue;
-    }, []);
+    return sentences.map((sentence, index) => {
+        return {
+            ...sentence,
+            group: clusters[index]
+        };
+    });
 };
--- src/utils/sentence-classification.js
+++ src/utils/sentence-classification.js
@@ -2,6 +2,6 @@ const embedding = require('./word-embedding');
 const classify = require('./k-means');
 
 module.exports = async (sentences, clusterCount) => {
-    const embeddings = await embedding(sentences);
+    const embeddings = await embedding(sentences.map((data) => data['sentence']));
     return classify(sentences, clusterCount, embeddings);
 };
